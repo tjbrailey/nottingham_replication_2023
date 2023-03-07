0c96c0814877d0f98dcb1ca675ad235d8fb88955
Thomas Brailey <thomasjbrailey@gmail.com>
2023-03-07 10:28:28 +0000

    Create xx_install_packages.R

---
+++ xx_install_packages.R
@@ -0,0 +1,7 @@
+###########################################################################
+##### Install packages required for replication and analysis
+
+
+# cjpowR is used in the original power analysis
+devtools::install_github("m-freitag/cjpowR")
+devtools::install_github('nathanvan/parallelsugar')
