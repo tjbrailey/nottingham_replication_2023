f57d133e87815ddac9d9df8324505e23ee2cac49
smoser11 <scott.moser@gmail.com>
2023-03-20 15:29:31 +0000

    Added Figure 1 replication, using only fist two tasks
    Creating figure1_replication2task.pdf

--- code/05_replicate_analysis.R
+++ code/05_replicate_analysis.R
@@ -170,6 +170,140 @@ ggsave(
   height = 15, width = 15)
 
 ###########################################################################
+##### Figure 1 -- but only using the first two profiles/ tasks
+
+### pooled model
+
+df_original_sub <- dplyr::filter(df_original, !is.na(support_rc))
+
+# run regression
+reg_f1_1t2 <- lm(
+  support_rc ~ candemmg_rc * cancom_rc,
+  data = filter(df_original_sub, task <=2) )
+
+texreg::screenreg(reg_f1_1t2)
+
+# calculate marginal effects
+reg_f1_1_meT2 <- ggeffects::ggeffect(
+  model = reg_f1_1t2, 
+  terms = c("candemmg_rc [0:1]", 
+            "cancom_rc [1:5 by = 1]"))
+
+# calculate contrasts
+reg_f1_1_emmT2 <- emmeans::emmeans(
+  object = reg_f1_1t2,
+  specs = ~ candemmg_rc * cancom_rc, 
+  vcov = sandwich::vcovCL(
+    x = reg_f1_1t2, 
+    cluster = df_original_sub$resp))
+
+reg_f1_1_cT2 <- emmeans::contrast(
+  object = reg_f1_1_emmT2, 
+  interaction = TRUE) %>%
+  as.data.frame(.) %>% 
+  dplyr::filter(candemmg_rc_eff == "0 effect") %>%
+  dplyr::mutate(
+    conf_low = estimate - (1.96 * SE),
+    conf_high = estimate + (1.96 * SE),
+    index = dplyr::row_number()) %>% 
+  dplyr::filter(cancom_rc_eff != "3 effect")
+
+mm_pooledT2 <- mm_plot(reg_f1_1_meT2, title = "Pooled", legend = TRUE)
+mm_pooledT2
+
+mm_c_pooledT2 <- mm_c_plot(reg_f1_1_cT2, title = NULL)
+mm_c_pooledT2
+
+### country subsets
+
+# countries to loop over
+vec_countries <- sort(unique(df_original$country))
+
+# store plots
+list_plotsT2 <- lapply(
+  X = seq_along(vec_countries), 
+  FUN = function(x){
+    
+    # create data subset
+    df_original_sub <- dplyr::filter(
+      df_original, 
+      country == vec_countries[x])
+    
+    # run regression
+    reg_f1_s <- lm(
+      support_rc ~ candemmg_rc * cancom_rc,
+      clusters = resp, 
+      data = dplyr::filter(
+        df_original, 
+        country == vec_countries[x]))
+    
+    # calculate marginal effects
+    reg_f1_s_me <- ggeffects::ggeffect(
+      model = reg_f1_s, 
+      terms = c("candemmg_rc", 
+                "cancom_rc [1:5 by = 1]"))
+    
+    # calculate contrasts
+    reg_f1_s_emm <- emmeans::emmeans(
+      reg_f1_s, 
+      ~ candemmg_rc * cancom_rc, 
+      type = "response")
+    
+    reg_f1_s_c <- emmeans::contrast(
+      reg_f1_s_emm, interaction = TRUE) %>%
+      as.data.frame(.) %>% 
+      dplyr::filter(candemmg_rc_eff == "0 effect") %>%
+      dplyr::mutate(
+        conf_low = estimate - 1.96 * SE,
+        conf_high = estimate + 1.96 * SE,
+        index = dplyr::row_number()) %>% 
+      dplyr::filter(cancom_rc_eff != "3 effect")
+    
+    # progress
+    print(paste("Done with", vec_countries[x]))
+    
+    # save
+    plots <- list()
+    plots[["marginals"]] <- mm_plot(reg_f1_s_me, vec_countries[x])
+    plots[["contrasts"]] <- mm_c_plot(reg_f1_s_c, title = NULL)
+    
+    return(plots)
+    
+  })
+
+### Combine all subpanels into final exhibit
+figure1t2 <- cowplot::plot_grid(
+  mm_pooled, 
+  list_plots[[1]]$marginals, 
+  list_plots[[2]]$marginals, 
+  
+  mm_c_pooled, 
+  list_plots[[1]]$contrasts, 
+  list_plots[[2]]$contrasts,
+  
+  list_plots[[3]]$marginals, 
+  list_plots[[4]]$marginals, 
+  list_plots[[5]]$marginals, 
+  
+  list_plots[[3]]$contrasts, 
+  list_plots[[4]]$contrasts, 
+  list_plots[[5]]$contrasts, 
+  
+  ncol = 3, 
+  rel_heights = c(1, 1/2, 1, 1/2, 1, 1/2, 1, 1/2), 
+  rel_widths = c(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1))
+
+figure1t2 
+# save plot
+ggsave(
+  plot = figure1t2, 
+  filename = paste0(
+    here::here("exhibits", "figures"), "/figure1_replication2task.pdf"),
+  height = 15, width = 15)
+
+
+
+###########################################################################
 ##### Appendix A
 
 df_original$cancom_rc2 <- relevel(df_original$cancom_rc, ref = "3")
