38ea76088fbced9ec526c9bb63ac042bc2dcbfd0
Thomas Brailey <thomasjbrailey@gmail.com>
2023-03-22 11:21:04 +0000

    updates analysis and extension

--- code/06_extend_analysis.R
+++ code/06_extend_analysis.R
@@ -102,21 +102,21 @@ response_times_100pct <- lapply(
 
 my_list <- list()
 for(i in c(10, 20, 30, 40, 100)){
-my_list[[as.character(i)]] <- lapply(X = 1:6, FUN = function(x){
-  estimate <- get(paste0("response_times_", i, "pct"))[[x]]$coefficients
-  std_error <- get(paste0("response_times_", i, "pct"))[[x]]$std.error
-  conf_low <- get(paste0("response_times_", i, "pct"))[[x]]$conf.low
-  conf_high <- get(paste0("response_times_", i, "pct"))[[x]]$conf.high
-  out <- data.frame(
-    "subset" = i,
-    "outcome" = vec_table_a1_columns[x], 
-    "estimate" = estimate,
-    "std_error" = std_error,
-    "conf_high" = conf_high,
-    "conf_low" = conf_low)
-  out %<>% tibble::rownames_to_column(var = "coef")
-  return(out)
-})
+  my_list[[as.character(i)]] <- lapply(X = 1:6, FUN = function(x){
+    estimate <- get(paste0("response_times_", i, "pct"))[[x]]$coefficients
+    std_error <- get(paste0("response_times_", i, "pct"))[[x]]$std.error
+    conf_low <- get(paste0("response_times_", i, "pct"))[[x]]$conf.low
+    conf_high <- get(paste0("response_times_", i, "pct"))[[x]]$conf.high
+    out <- data.frame(
+      "subset" = i,
+      "outcome" = vec_table_a1_columns[x], 
+      "estimate" = estimate,
+      "std_error" = std_error,
+      "conf_high" = conf_high,
+      "conf_low" = conf_low)
+    out %<>% tibble::rownames_to_column(var = "coef")
+    return(out)
+  })
 }
 
 df_response_times <- dplyr::bind_rows(my_list)
@@ -161,13 +161,55 @@ reg_olog1 <- MASS::polr(as.factor(support) ~ candemmg + cancom, data = df_origin
 brant::brant(reg_olog1) # fails
 
 # multinomial logit
-reg_mlog1 <- nnet::multinom(as.factor(support) ~ candemmg + cancom, 
+reg_mlog1 <- nnet::multinom(support ~ candemmg * cancom, 
                             data = df_original)
 
-# tabulate outcome
-reg_mlog1 %>% 
-  gtsummary::tbl_regression(exponentiate = TRUE) %>% 
-  gtsummary::as_kable_extra(tbl, format = "latex")
+# calculate predicted probabilities
+df_mlog1 <- ggeffects::ggeffect(
+  terms = c("candemmg", "cancom"),
+  model = reg_mlog1)
+
+# plot outcome
+df_mlog1 %>% 
+  tibble::as_tibble() %>% 
+  dplyr::mutate(
+    response.level = dplyr::case_when(
+      response.level == "Neither.likely.nor.unlikely" ~ 3,
+      response.level == "Somewhat.likely" ~ 4,
+      response.level == "Very.likely" ~ 5,
+      response.level == "Very.unlikely" ~ 1,
+      response.level == "Somewhat.unlikely" ~ 2,
+      TRUE ~ NA_real_),
+    group = dplyr::case_when(
+      group == "Neither competent nor incompetent" ~ 3,
+      group == "Moderately competent" ~ 4,
+      group == "Very competent" ~ 5,
+      group == "Very incompetent" ~ 1,
+      group == "Moderately incompetent" ~ 2,
+      TRUE ~ NA_real_)) %>% 
+  dplyr::filter(!is.na(response.level)) %>% 
+  ggplot(data = ., 
+         mapping = aes(x = group, y = predicted, color = x)) + 
+  geom_point() + 
+  geom_line() + 
+  facet_wrap(. ~ response.level, labeller = as_labeller(
+    c("1" = "Very Unlikely",
+      "2" = "Somewhat Unlikely", 
+      "3" = "Neither Likely nor Unlikely", 
+      "4" = "Somewhat Likely",
+      "5" = "Very Likely"))) +
+  scale_color_grey() + 
+  labs(x = "Competence", y = "Predicted Probability", color = "") + 
+  #scale_x_continuous(labels = c("1" = "Very Incompetent",
+  #                              "2" = "Moderately Incompetent", 
+  #                              "3" = "Neither Competent Nor Incompetent",
+  #                              "4" = "Moderately Competent", 
+  #                              "5" = "Very Competent"))
+  theme_bw() +
+  theme(aspect.ratio = 1,
+        legend.position = "bottom")
+ggsave(plot = last_plot(),
+       filename = paste0(here::here("exhibits", "figures", "mlogit_pp.pdf")))
 
 # example candidate
 eg <- dplyr::filter(df_original, country == "CZ", respondentid %in% 4:5)
@@ -231,8 +273,13 @@ df_original %>%
     mean = mean(diff, na.rm = TRUE)) %>% 
   ggplot(., mapping = aes(x = task, y = mean, color = country)) +
   geom_line() + 
+  scale_x_continuous(
+    breaks = seq(1, 10, 1),
+    labels = seq(1, 10, 1)) +
+  labs(x = "Task Number", 
+       y = "Mean Difference in Choice\nBetween Candidates",
+       color = "") +
   theme_bw() +
-  labs(x = "Task Number", y = "Mean Difference in Choice\nBetween Candidates") +
   theme(aspect.ratio = 1, 
         legend.position = "bottom")
 
@@ -246,11 +293,19 @@ df_original %>%
     mean = mean(diff, na.rm = TRUE)) %>% 
   ggplot(., mapping = aes(x = task, y = mean, color = country)) +
   geom_line() + 
+  scale_x_continuous(
+    breaks = seq(1, 10, 1),
+    labels = seq(1, 10, 1)) +
+  labs(x = "Task Number", 
+       y = "Mean Difference in Choice\nBetween Candidates",
+       color = "") +
   theme_bw() +
-  labs(x = "Task Number", y = "Mean Difference in Choice\nBetween Candidates") +
   theme(aspect.ratio = 1, 
         legend.position = "bottom")
+ggsave(plot = last_plot(),
+       filename = here::here("exhibits", "figures", "diff_in_candidate_choice.pdf"))
 
+# pooled
 df_original %>% 
   dplyr::group_by(respondentid, task) %>% 
   dplyr::summarise(
@@ -259,14 +314,23 @@ df_original %>%
   dplyr::summarise(
     mean = mean(diff, na.rm = TRUE)) %>%
   ggplot(., mapping = aes(x = task, y = mean)) +
-  geom_line() + 
-  theme_bw() +
+  geom_line() +
+  scale_x_continuous(
+    breaks = seq(1, 10, 1),
+    labels = seq(1, 10, 1)) +
   labs(x = "Task Number", y = "Mean Difference in Choice\nBetween Candidates") +
+  theme_bw() +
   theme(aspect.ratio = 1, 
         legend.position = "bottom")
 
-estimatr::lm_robust(
-  support_rc ~ candemmg_rc2 * cancom_rc2, 
-  data = dplyr::filter(df_original, task == 5))
+df_original %<>%
+  dplyr::group_by(respondentid, task) %>% 
+  dplyr::mutate(
+    cand_pair_diff = abs(
+      dplyr::first(support_rc) - dplyr::last(support_rc))) %>% 
+  dplyr::ungroup()
 
-  
+estimatr::lm_robust(
+  cand_pair_diff ~ as.factor(task), 
+  data = df_original)
+
