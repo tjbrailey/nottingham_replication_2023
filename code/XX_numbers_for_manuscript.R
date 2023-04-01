e11817073403c0043d4ca1eaaa86657040ee757c
Thomas Brailey <thomasjbrailey@gmail.com>
2023-04-02 00:46:13 +0100

    Update XX_numbers_for_manuscript.R

--- code/XX_numbers_for_manuscript.R
+++ code/XX_numbers_for_manuscript.R
@@ -1,5 +1,11 @@
+###########################################################################
+##### Numbers referenced in manuscript
 
+# If we need to calculate a certain statistic to use in the article, here 
+# is where we calculate it. Once the manuscript is complete, we will 
+# include a comment on the page number that the statistic is used.
 
+# duration of responses by country
 df_original %>% 
   dplyr::group_by(country) %>% 
   dplyr::summarise(
@@ -8,6 +14,7 @@ df_original %>%
     median = median(durationinseconds, na.rm = TRUE),
     n    = dplyr::n())
 
+# global average of duration of response
 df_original %>% 
   dplyr::summarise(
     mean = round(mean(durationinseconds, na.rm = TRUE)),
